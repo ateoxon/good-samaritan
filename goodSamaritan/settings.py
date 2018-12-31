5f5a994e75092bc472f4364a2db4859184769a33
Angelo Teoxon <ateoxon97@gmail.com>
2018-12-31 17:19:21 -0600

    added static settings for heroku

--- goodSamaritan/settings.py
+++ goodSamaritan/settings.py
@@ -133,3 +133,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+#########
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
