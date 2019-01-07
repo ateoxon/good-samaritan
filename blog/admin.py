cc86012084f24aed7ff4a75646e1b8ceed43c533
Angelo Teoxon <ateoxon97@gmail.com>
2019-01-07 17:01:27 -0600

    update profile

--- blog/admin.py
+++ blog/admin.py
@@ -5,3 +5,8 @@ from django.contrib import admin
 
 from .models import Post
 admin.site.register(Post) #this should register Posts db and should show
+
+
+admin.site.site_header = "Good Samaritan"
+admin.site.site_title = "Good Samaritan"
+admin.site.index_title = "Good Samaritan Site Administration"
