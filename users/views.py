a2de42e845333e1b2b19e979c6018c529ce1302d
Angelo Teoxon <ateoxon97@gmail.com>
2019-02-11 12:46:36 -0600

    add email activation to views and separate file

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -11,8 +12,16 @@ from django.views.generic import (
 )
 from inventory.models import *
 
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+
 from django.core.mail import send_mail
 from django.conf import settings
+
+from django.core.mail import EmailMessage
 
 
 def landing(request):
@@ -24,7 +33,25 @@ def register(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             form.save()
-            messages.success(request, f'Your account has been created! You are now able to log in')
+
+            mail_subject = 'Activate your Good Samaritan Account!'
+            mail_message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[user.email]
+            )
+            email.send()
+
+            #return HttpResponse('Please confirm your email address to complete the registration')
+
+
+            messages.success(request, f'Confirm your email to complete registration!')
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -57,3 +84,20 @@ def editProfile(request):
         'p_form': p_form
     }
     return render(request, 'users/editProfile.html', context)
+
+
+#activation confirmation
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
